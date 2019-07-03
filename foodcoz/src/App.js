c6dedce54732fe60c7d988de3cecf3f18e073515
Thanakit Yannam <thanakit.ya@ku.th>
2019-07-03 23:01:13 +0700

    addMenuEditPage.js

--- foodcoz/src/App.js
+++ foodcoz/src/App.js
@@ -5,18 +5,21 @@ import OrderPage from './OrderPage'
 import Login from './Login'
 import MenuPage from './MenuPage';
 import MEditMenu from './MEditMenu'
+import MenuEditPage from './pages/MenuEditPage'
+// import Test from './pages/Test'
 // import totalbill from './totalbill.html'
 
 const App = () => {
   return(
     <Router >
         <div>
-        <Route path="/" exact component={Login}/>
-        <Route path="/orderPage" component={OrderPage}/>
-        <Route path="/menuPage" component={MenuPage}/>
-        {/* <Route path="/editTable" component={EditTable}/> */}
-        <Route path="/meditMenu" component={MEditMenu}/>
-        {/* <Route path="/totalbill" component={totalbill}/> */}
+          <Route path="/" exact component={Login}/>
+          <Route path="/orderPage" component={OrderPage}/>
+          <Route path="/menuPage" component={MenuPage}/>   {/* <------ didn't use*/}
+          {/* <Route path="/editTable" component={EditTable}/> */}
+          <Route path="/meditMenu" component={MEditMenu}/>    
+          <Route path="/menueditpage" component={MenuEditPage}/>
+          {/* <Route path="/totalbill" component={totalbill}/> */}
         </div>
     </Router>
         
