5d5d7e18d369817fa0114b993c607a965d0e324d
Woraton Sartsangchan <woraton.s@ku.th>
2019-07-04 13:59:15 +0700

    update login -> meditMenu

--- foodcoz/src/Login.js
+++ foodcoz/src/Login.js
@@ -4,57 +4,53 @@ import './Login.css';
 import firebase from './firebase';
 export default class Login extends Component {
 
-//   constructor(props) {
-//     super(props)
-//     this.state = { email: '', password: '' }
-//     this.handleChange = this.handleChange.bind(this)
-//     this.handleSubmit = this.handleSubmit.bind(this)
-//   }
+  //   constructor(props) {
+  //     super(props)
+  //     this.state = { email: '', password: '' }
+  //     this.handleChange = this.handleChange.bind(this)
+  //     this.handleSubmit = this.handleSubmit.bind(this)
+  //   }
 
-//   handleChange(event) {
-//     this.setState({ email: event.target.value })
-//   }
+  //   handleChange(event) {
+  //     this.setState({ email: event.target.value })
+  //   }
 
-//   handleSubmit(event) {
-//     alert(this.state.email)
-//     event.preventDefault()
-//   }
+  //   handleSubmit(event) {
+  //     alert(this.state.email)
+  //     event.preventDefault()
+  //   }
 
-//   firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-//   // Handle Errors here.
-//   var errorCode = error.code;
-//   var errorMessage = error.message;
-//   if (errorCode === 'auth/wrong-password') {
-//     alert('Wrong password.');
-//   } else {
-//     alert(errorMessage);
-//   }
-//   console.log(error);
-// });
-
-  
+  //   firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+  //   // Handle Errors here.
+  //   var errorCode = error.code;
+  //   var errorMessage = error.message;
+  //   if (errorCode === 'auth/wrong-password') {
+  //     alert('Wrong password.');
+  //   } else {
+  //     alert(errorMessage);
+  //   }
+  //   console.log(error);
+  // });
 
 
-    render() {
-        return (
-            <div className="App">
-       <div>
-         <h1>FoodCOZ</h1>
-       </div>
-      
-       <div height="200">
-         <lebal className="textLebal" >ID</lebal>
-       </div>
 
-       <Form.Control type="email" className="inputPlace" />
 
-       <div>
-         <lebal className="textLebal" >PASSWORD</lebal>
-       </div>
+  render() {
+    firebase.auth().currentUser
+    return (
+      <div className="App">
+        <div>
+          <h1>FoodCOZ</h1>
+        </div>
 
-       <Form.Control type="email" className="inputPlace" data-init/>
+        <div height="200">
+          <lebal className="textLebal" >ID</lebal>
+        </div>
+
+        <Form.Control type="email" className="inputPlace" />
 
         <div>
+          <lebal className="textLebal" >PASSWORD</lebal>
         </div>
           <div height="1000">
        <Button className="loginButton" variant="success" href="/meditMenu">LOG IN</Button>
